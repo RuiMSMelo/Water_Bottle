1a1a0a9ae8ddac351a56556fd3d0d644c07b72b7
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-26 15:33:40 +0100

    added model

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import { useGLTF } from '@react-three/drei'
+import { Suspense, useEffect } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Bottle from '../public/Waterbottle-v1-model.jsx'
+
+function WaterBottle() {
+    return <Bottle />
+}
 
 function App() {
-    const waterBottle = useGLTF
-
     return (
         <>
-            <p>hello</p>
+            <Canvas>
+                <color attach='background' args={['tomato']} />
+                <ambientLight intensity={2} />
+                <spotLight position={[3, 4, 3]} intensity={3} />
+                <Suspense fallback={null}>
+                    <Bottle position={[0, -2, 0]} />
+                </Suspense>
+            </Canvas>
         </>
     )
 }
