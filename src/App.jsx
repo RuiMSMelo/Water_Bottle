482c39d2262c55405172b134849b8775a4514888
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-26 19:46:07 +0100

    fixed css styling problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { useGLTF } from '@react-three/drei'
-import { Suspense, useEffect } from 'react'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Bottle from '../public/Waterbottle-v1-model.jsx'
 
@@ -10,16 +10,14 @@ function WaterBottle() {
 
 function App() {
     return (
-        <>
-            <Canvas>
-                <color attach='background' args={['tomato']} />
-                <ambientLight intensity={2} />
-                <spotLight position={[3, 4, 3]} intensity={3} />
-                <Suspense fallback={null}>
-                    <Bottle position={[0, -2, 0]} />
-                </Suspense>
-            </Canvas>
-        </>
+        <Canvas>
+            <OrbitControls />
+            <ambientLight intensity={0.25} />
+            <spotLight position={[3, 4, 3]} intensity={10} />
+            <Suspense fallback={null}>
+                <Bottle position={[0, -2, 0]} />
+            </Suspense>
+        </Canvas>
     )
 }
 
